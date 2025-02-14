aa3660a4085d445ebe59040d907155eeacbdec7b
techy18ai <deepghosh56789@gmail.com>
2025-02-14 13:27:49 +0530

    python-app.yml done

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,11 +1,11 @@
-from src.maths_operation import add,sub 
+from src.maths_operation import add
+from src.maths_operation import sub
 
 def test_add(): 
-    assert add(5,3) == 8 
-    assert add(-1,1)==0 
-    assert add(-1,-1)==-2   
-    
-def test_sub(): 
-    assert sub(5,3) == 2
-    assert sub(3,3) == 0
+    assert add(5, 3)==8
+    assert add(-1, 1)==0
+    assert add(-1, -1)==-2
 
+def test_sub(): 
+    assert sub(5, 3)==2
+    assert sub(3, 3)==0
